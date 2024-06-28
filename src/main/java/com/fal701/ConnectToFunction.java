0de89f745281cd4653551a8fe20a266d9279ae48
lolra <raphaelharvey15@gmail.com>
2024-06-28 00:53:42 -0400

    Started connected textures

--- src/main/java/com/fal701/ConnectToFunction.java
+++
@@ -1,8 +0,0 @@
-package com.fal701;
-
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.Direction;
-
-public interface ConnectToFunction {
-    public boolean apply(BlockState from, BlockState to, Direction dir);
-}
